bd96d50912718b0d58abbba11478b8fd8391a402
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-06-20 18:38:45 +0530

    Nav bar

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="page-content hero">
+      <h1>
+        Contact <sup>(01)</sup>
+      </h1>
+    </div>
+  );
+}
---
+++ src/app/work/page.jsx
@@ -0,0 +1,9 @@
+export default function Work() {
+  return (
+    <div className="page-content hero">
+      <h1>
+        Work <sup>(03)</sup>
+      </h1>
+    </div>
+  );
+}
